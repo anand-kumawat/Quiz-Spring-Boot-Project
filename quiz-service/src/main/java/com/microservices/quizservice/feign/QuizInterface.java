97469d8b993dccf9e3ccf3042554537f5cc9fea6
anand-kumawat <132658480+anand-kumawat@users.noreply.github.com>
2024-03-05 15:53:37 +0530

    Inital Commit

---
+++ quiz-service/src/main/java/com/microservices/quizservice/feign/QuizInterface.java
@@ -0,0 +1,27 @@
+package com.microservices.quizservice.feign;
+
+import com.microservices.quizservice.model.QuestionWrapper;
+import com.microservices.quizservice.model.Response;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient("QUESTION-SERVICE")  // we need to add here that which service we want to call from this service
+// and most importantly the name of the service should be in capital as this is registered in the eureka server.
+public interface QuizInterface {
+    @GetMapping("question/generate")
+    public ResponseEntity<List<Integer>> getQuestionsForQuiz
+            (@RequestParam String categoryName, @RequestParam Integer numQuestions );
+
+    @PostMapping("question/getQuestions")
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@RequestBody List<Integer> questionIds);
+
+    @PostMapping("question/getScore")
+    public ResponseEntity<Integer> getScore(@RequestBody List<Response> responses);
+
+}
